627eafe44a56232d5d58da7bcbc7a242d750b33d
Krishna_KS <krishna_ks@gmail.com>
2026-07-14 17:19:12 -0500

    Add .NET application GitHub Actions workflow

---
+++ dotnet-app/Program.cs
@@ -0,0 +1,3 @@
+Console.WriteLine("Hello from my .NET application!");
+Console.WriteLine("The application is running through GitHub Actions.");
+Console.WriteLine("The .NET build and execution completed successfully.");
